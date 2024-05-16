380499b37b95e9c3175a31b4a914e4f65563567a
James M Giraldo <jmichaelgiraldo@gmail.com>
2024-05-15 21:13:34 -0500

    Submitting Completed Assigment

--- grade_calculator.py
+++ grade_calculator.py
@@ -1,15 +1,14 @@
+#Student Average Grade Calculator by James Giraldo
+
 def calculate_average_grade():
-    # Prompt the user for their name and store it in the student_name variable
-    student_name = ""
+    # This section prompts the user for their name and scores
+    student_name = input("Hi! Welcome to the grade average calculator! May I have your name, please? ")
+    math_score = float(input("Thank you! What is your math score? "))
+    science_score = float(input("Great! What is your science score? "))
+    english_score = float(input("Finally, what is your english score? "))
 
-    # Prompt the user for their scores in Math, Science, and English
-    # Store the scores in the respective variables: math_score, science_score, english_score
-    math_score = 0
-    science_score = 0
-    english_score = 0
-
-    # Calculate the average grade
-    average_grade = 0
+    # This calculates the student's average test scores
+    average_grade = ((math_score+science_score+english_score)/3)
 
     # Return the student's name and their average grade
     return student_name, average_grade
@@ -18,5 +17,5 @@ if __name__ == '__main__':
     # Call the calculate_average_grade function
     student_name, average_grade = calculate_average_grade()
 
-    # Print the student's name and their average grade
-    print(f"")
+    # This will output the student's name and average grade rounded to 2 decimal places
+    print(f'{student_name } your average grade is {average_grade:.2f}!')
